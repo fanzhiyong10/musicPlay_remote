b868b1460654717e5a5137acf93e7d2ed973532f
fanzhiyong10 <13700318430@163.com>
2022-07-21 21:28:24 +0800

    添加 第二个label

--- musicPlay/ViewController.swift
+++ musicPlay/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var secondLabel: UILabel!
     var testLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +19,16 @@ class ViewController: UIViewController {
     }
 
     func createLabel() {
-        var aRect = CGRect(x: 10, y: 10, width: 200, height: 30)
+        var aRect = CGRect(x: 10, y: 60, width: 200, height: 30)
         testLabel = UILabel(frame: aRect)
         testLabel.text = "测试label"
         self.view.addSubview(self.testLabel)
+        
+        
+        aRect.origin.y += 50
+        secondLabel = UILabel(frame: aRect)
+        secondLabel.text = "测试: 第二个 label"
+        self.view.addSubview(self.secondLabel)
     }
 
 }
